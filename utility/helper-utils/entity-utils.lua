852f6e95f9cf07d757dbe63c443cc6442c54697b
Muppet9010 <mike.hollett@gmail.com>
2022-11-11 19:20:28 +0000

    Entity Utils
    Add in a default move things out of the way function.
    Tidy up the other functions and add some documentation to them.
    Reviewed their UPS cost and made small tweaks, plus added some notes on reviews done.
    No real functional changes to existing usage situations.

--- utility/helper-utils/entity-utils.lua
+++ utility/helper-utils/entity-utils.lua
@@ -6,6 +6,9 @@
 local EntityUtils = {} ---@class Utility_EntityUtils
 local PositionUtils = require("utility.helper-utils.position-utils")
 
+local MovablePrototypeTypes = { unit = "unit", character = "character", car = "car", tank = "tank", ["spider-vehicle"] = "spider-vehicle" }
+
+--- Returns all objects in the area that match the very specific requirements.
 ---@param surface LuaSurface
 ---@param positionedBoundingBox BoundingBox
 ---@param collisionBoxOnlyEntities boolean
@@ -15,6 +18,7 @@ local PositionUtils = require("utility.helper-utils.position-utils")
 ---@param entitiesExcluded? LuaEntity[]
 ---@return table<int, LuaEntity>
 EntityUtils.ReturnAllObjectsInArea = function(surface, positionedBoundingBox, collisionBoxOnlyEntities, onlyForceAffected, onlyDestructible, onlyKillable, entitiesExcluded)
+    -- CODE NOTE: Looked at using filtered search, but it would only eliminate a few odd prototype types like smoke or projectiles. I would still need to run all of the field checks against the entities found as the individual prototype or entity may have a non default state set on it and I'd want to respect that.
     local entitiesFound = surface.find_entities(positionedBoundingBox)
     local filteredEntitiesFound = {} ---@type table<int, LuaEntity>
     for _, entity in pairs(entitiesFound) do
@@ -30,8 +34,8 @@ EntityUtils.ReturnAllObjectsInArea = function(surface, positionedBoundingBox, co
         if not entityExcluded then
             if (onlyForceAffected == nil) or (entity.force == onlyForceAffected) then
                 if (not onlyDestructible) or (entity.destructible) then
-                    if (not onlyKillable) or (entity.health ~= nil) then
-                        if (not collisionBoxOnlyEntities) or (PositionUtils.IsBoundingBoxPopulated(entity.prototype.collision_box)) then
+                    if (not onlyKillable) or (entity.health ~= nil) then -- This will kill something that has 0 health set by a mod. As this is an edge case and the thing is killable according to the prototype, the destructible check should protect against sensible situations around this.
+                        if (not collisionBoxOnlyEntities) or (PositionUtils.IsBoundingBoxPopulated(entity.bounding_box)) then
                             filteredEntitiesFound[#filteredEntitiesFound + 1] = entity
                         end
                     end
@@ -42,6 +46,7 @@ EntityUtils.ReturnAllObjectsInArea = function(surface, positionedBoundingBox, co
     return filteredEntitiesFound
 end
 
+--- Kills everything in the area that can be killed, the rest is ignored.
 ---@param surface LuaSurface
 ---@param positionedBoundingBox BoundingBox
 ---@param killerEntity? LuaEntity
@@ -62,6 +67,7 @@ EntityUtils.KillAllKillableObjectsInArea = function(surface, positionedBoundingB
     end
 end
 
+--- Kills everything in the area that can be killed, and destroy the rest.
 ---@param surface LuaSurface
 ---@param positionedBoundingBox BoundingBox
 ---@param killerEntity? LuaEntity
@@ -73,7 +79,7 @@ EntityUtils.KillAllObjectsInArea = function(surface, positionedBoundingBox, kill
         killerForce = "neutral"
     end
     for k, entity in pairs(EntityUtils.ReturnAllObjectsInArea(surface, positionedBoundingBox, false, onlyForceAffected, false, false, entitiesExcluded)) do
-        if entity.destructible then
+        if entity.destructible and entity.health ~= nil then
             if killerEntity ~= nil then
                 entity.die(killerForce, killerEntity)
             else
@@ -85,23 +91,25 @@ EntityUtils.KillAllObjectsInArea = function(surface, positionedBoundingBox, kill
     end
 end
 
+--- Destroys (removes) everything in the area that is killable.
 ---@param surface LuaSurface
 ---@param positionedBoundingBox BoundingBox
 ---@param collisionBoxOnlyEntities boolean
 ---@param onlyForceAffected? LuaForce
 ---@param entitiesExcluded? LuaEntity[]
 EntityUtils.DestroyAllKillableObjectsInArea = function(surface, positionedBoundingBox, collisionBoxOnlyEntities, onlyForceAffected, entitiesExcluded)
-    for k, entity in pairs(EntityUtils.ReturnAllObjectsInArea(surface, positionedBoundingBox, collisionBoxOnlyEntities, onlyForceAffected, true, true, entitiesExcluded)) do
+    for _, entity in pairs(EntityUtils.ReturnAllObjectsInArea(surface, positionedBoundingBox, collisionBoxOnlyEntities, onlyForceAffected, true, true, entitiesExcluded)) do
         entity.destroy { do_cliff_correction = true, raise_destroy = true }
     end
 end
 
+--- Destroys (removes) everything in the area regardless of if it is killable. This will catch all entity types, including smoke and other things.
 ---@param surface LuaSurface
 ---@param positionedBoundingBox BoundingBox
 ---@param onlyForceAffected? LuaForce
 ---@param entitiesExcluded? LuaEntity[]
 EntityUtils.DestroyAllObjectsInArea = function(surface, positionedBoundingBox, onlyForceAffected, entitiesExcluded)
-    for k, entity in pairs(EntityUtils.ReturnAllObjectsInArea(surface, positionedBoundingBox, false, onlyForceAffected, false, false, entitiesExcluded)) do
+    for _, entity in pairs(EntityUtils.ReturnAllObjectsInArea(surface, positionedBoundingBox, false, onlyForceAffected, false, false, entitiesExcluded)) do
         entity.destroy { do_cliff_correction = true, raise_destroy = true }
     end
 end
@@ -115,6 +123,34 @@ EntityUtils.EntityDie = function(entity, killerForce, killerCauseEntity)
         entity.die(killerForce, killerCauseEntity)
     else
         entity.die(killerForce)
+    end
+end
+
+--- Move any teleportable entities in the bounding box of an entity out of the way. Anything non movable can be just killed, `die()` with the created entity as the killer.
+---@param surface LuaSurface
+---@param centralEntity LuaEntity
+---@param killNonMoved boolean
+---@param createdEntityForce? LuaForce # The force that will do the kill on any unmoved entity if `killNonMoved == true`. If not provided and needed it is obtained.
+EntityUtils.MoveKillableObjectsFromEntityBoundingBox = function(surface, centralEntity, killNonMoved, createdEntityForce)
+    local entitiesInTheWay = EntityUtils.ReturnAllObjectsInArea(surface, centralEntity.bounding_box, true, nil, true, true, { centralEntity })
+    if #entitiesInTheWay == 0 then return end
+
+    if killNonMoved then
+        createdEntityForce = createdEntityForce or centralEntity.force --[[@as LuaForce]]
+    end
+    local entityMoved, entityNewPosition
+    for _, entity in pairs(entitiesInTheWay) do
+        entityMoved = false
+        if MovablePrototypeTypes[entity.type] ~= nil then
+            entityNewPosition = surface.find_non_colliding_position(entity.name, entity.position, 2, 0.1)
+            if entityNewPosition ~= nil then
+                entity.teleport(entityNewPosition)
+                entityMoved = true
+            end
+        end
+        if not entityMoved and killNonMoved then
+            entity.die(createdEntityForce, centralEntity)
+        end
     end
 end
 
